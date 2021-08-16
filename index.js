5bd28261a30181f68e96f89990e36cd33568722c
seyedalian <seyedalian@outlook.com>
2021-08-16 13:39:18 +0430

    node js app server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var fs = require('fs');
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname)); 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(port, () => {
+  console.log(`node js app server listening at http://localhost:${port}`)
+})
